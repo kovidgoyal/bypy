b3ff8c2fd1f714d785d8eaefbf526ed85e54cb1f
Kovid Goyal <kovid@kovidgoyal.net>
2019-05-02 12:42:44 +0530

    Port Qt

--- bypy/pkgs/qt.py
+++ bypy/pkgs/qt.py
@@ -1,67 +1,73 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
 import os
 import shutil
 
-from bypy.constants import CFLAGS, LDFLAGS, MAKEOPTS, build_dir, ismacos, islinux, LIBDIR, iswindows, PREFIX
-from bypy.utils import apply_patch, run, run_shell, replace_in_file, ModifiedEnv, current_env
+from bypy.constants import (CFLAGS, LDFLAGS, LIBDIR, MAKEOPTS, PREFIX,
+                            build_dir, islinux, ismacos, iswindows)
+from bypy.utils import (ModifiedEnv, current_env, replace_in_file, run,
+                        run_shell)
 
 
 def main(args):
     if islinux:
         # We disable loading of bearer plugins because many distros ship with
-        # broken bearer plugins that cause hangs.  At least, this was the case in
-        # Qt 4.x Dont know if it is still true for Qt 5 but since we dont need
-        # bearers anyway, it cant hurt.
-        replace_in_file('qtbase/src/network/bearer/qnetworkconfigmanager_p.cpp', b'/bearer"', b'/bearer-disabled-by-kovid"')
+        # broken bearer plugins that cause hangs.  At least, this was the case
+        # in Qt 4.x Dont know if it is still true for Qt 5 but since we dont
+        # need bearers anyway, it cant hurt.
+        replace_in_file(
+            'qtbase/src/network/bearer/qnetworkconfigmanager_p.cpp',
+            b'/bearer"', b'/bearer-disabled-by-kovid"')
         # Change pointing_hand to hand2, see
         # https://bugreports.qt.io/browse/QTBUG-41151
-        replace_in_file('qtbase/src/plugins/platforms/xcb/qxcbcursor.cpp', 'pointing_hand"', 'hand2"')
+        replace_in_file('qtbase/src/plugins/platforms/xcb/qxcbcursor.cpp',
+                        'pointing_hand"', 'hand2"')
     elif iswindows:
         # Enable loading of DLLs from the DLLs directory
         replace_in_file(
             'qtbase/src/corelib/plugin/qsystemlibrary.cpp',
             'searchOrder << QFileInfo(qAppFileName()).path();',
-            r'''searchOrder << (QFileInfo(qAppFileName()).path().replace(QLatin1Char('/'), QLatin1Char('\\')) + QString::fromLatin1("\\app\\DLLs\\"));''')
+            'searchOrder << (QFileInfo(qAppFileName()).path()'
+            r".replace(QLatin1Char('/'), QLatin1Char('\\'))"
+            r'+ QString::fromLatin1("\\app\\DLLs\\"));'
+        )
     cflags, ldflags = CFLAGS, LDFLAGS
     if ismacos:
-        apply_patch('fix_mojave_font_weights_in_qt_5.6.patch')
         ldflags = '-L' + LIBDIR
-        # The following is needed as without it the Qt build system does not add the
-        # necessary -stdlib=libc++ when linking. Probably, you can get rid of
-        # this on newer OS X systems/newer Qt versions
-        replace_in_file('qtbase/mkspecs/macx-clang/qmake.conf', 'MACOSX_DEPLOYMENT_TARGET = 10.7', 'MACOSX_DEPLOYMENT_TARGET = 10.9')
     os.mkdir('build'), os.chdir('build')
-    configure = os.path.abspath('..\\configure.bat') if iswindows else '../configure'
+    configure = os.path.abspath(
+        '..\\configure.bat') if iswindows else '../configure'
     # Slim down Qt
     # For the list of modules and their dependencies, see .gitmodules
-    skip_modules = (
-        # To add web engine remove qtwebengine and qtwebview from this list
-        'qtdeclarative qtactiveqt qtscript qttools qtxmlpatterns qttranslations qtdoc'
-        ' qt3d qtgraphicaleffects qtquickcontrols qtquickcontrols2 qtwebengine qtwebview'
-        ' qtcanvas3d'
-    ).split()
+    skip_modules = set('''\
+        qtactiveqt qtscript qttools qtxmlpatterns qttranslations qtdoc
+        qtrepotools qtqa qtlocation qtsensors qtsystems qtfeedback qtdocgallery
+        qtpim qtconnectivity qt3d qtgraphicaleffects qtquickcontrols
+        qtserialbus qtserialport qtquickcontrols2 qtcanvas3d qtpurchasing
+        qtcharts qtdatavis3d qtvirtualkeyboard qtgamepad qtscxml qtspeech
+        qtremoteobjects qtwebglplugin
+    '''.split())
     conf = configure + (
-        ' -v -silent -opensource -confirm-license -prefix {}/qt -release -nomake examples -nomake tests'
-        ' -no-sql-odbc -no-sql-psql -no-qml-debug -icu -qt-harfbuzz'
-    ).format(build_dir())
+        ' -v -silent -opensource -confirm-license -prefix {}/qt -release'
+        ' -nomake examples -nomake tests -no-sql-odbc -no-sql-psql'
+        ' -icu -qt-harfbuzz -qt-doubleconversion').format(build_dir())
     if islinux:
-        # Ubuntu 12.04 has gcc 4.6.3 which does not support c++11
-        conf += ' -qt-xcb -glib -openssl -gtkstyle -qt-pcre -c++std c++98'
+        conf += (
+            ' -qt-xcb -glib -openssl -qt-pcre -xkbcommon -libinput'
+        )
     elif ismacos:
-        # Use c++11 rather than the newest available for maximum backwards
-        # compat
-        conf += ' -no-pkg-config -framework -no-openssl -securetransport -no-freetype -no-fontconfig -c++std c++11'
+        conf += ' -no-pkg-config -framework -no-openssl -securetransport'
+        ' -no-freetype -no-fontconfig '
     elif iswindows:
-        # Qt links incorrectly against libpng and libjpeg, so use the bundled copy
-        # Use dynamic OpenGl, as per:
+        # Qt links incorrectly against libpng and libjpeg, so use the bundled
+        # copy Use dynamic OpenGl, as per:
         # https://doc.qt.io/qt-5/windows-requirements.html#dynamically-loading-graphics-drivers
-        conf += (' -openssl -directwrite -ltcg -platform win32-msvc2015 -mp -no-plugin-manifests -no-freetype -no-fontconfig'
-                 ' -no-angle -opengl dynamic -qt-libpng -qt-libjpeg ')
+        conf += (
+            ' -openssl -directwrite -ltcg -platform win32-msvc2015 -mp'
+            ' -no-plugin-manifests -no-freetype -no-fontconfig'
+            ' -no-angle -opengl dynamic -qt-libpng -qt-libjpeg ')
         # The following config items are not supported on windows
         conf = conf.replace('-v -silent ', ' ')
         cflags = '-I {}/include'.format(PREFIX).replace(os.sep, '/')
@@ -72,11 +78,14 @@ def main(args):
     # run_shell()
     run_shell
     if iswindows:
-        with ModifiedEnv(PATH=os.path.abspath('../gnuwin32/bin') + os.pathsep + current_env()['PATH']):
+        with ModifiedEnv(PATH=os.path.abspath('../gnuwin32/bin') + os.pathsep +
+                         current_env()['PATH']):
             run('nmake')
         run('nmake install')
-        shutil.copy2('../qtbase/src/3rdparty/sqlite/sqlite3.c', os.path.join(build_dir(), 'qt'))
-        shutil.copytree('../gnuwin32', os.path.join(build_dir(), 'qt', 'gnuwin32'))
+        shutil.copy2('../qtbase/src/3rdparty/sqlite/sqlite3.c',
+                     os.path.join(build_dir(), 'qt'))
+        shutil.copytree('../gnuwin32',
+                        os.path.join(build_dir(), 'qt', 'gnuwin32'))
     else:
         run('make ' + MAKEOPTS, library_path=True)
         run('make install')
