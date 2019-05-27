5ca33e394e42db0e484e6aa8676b77589118f3c2
Kovid Goyal <kovid@kovidgoyal.net>
2019-05-27 20:20:00 +0530

    Use HEAD to build qt-webkit
    Upstream has several needed patches but shows no signs of wanting to
    make a release.

--- bypy/pkgs/webkit.py
+++ bypy/pkgs/webkit.py
@@ -14,13 +14,6 @@ patch = partial(apply_patch, convert_line_endings=iswindows)
 def main(args):
     # Control font hinting
     patch('webkit_control_hinting.patch')
-    # Fix null point dereference (Fedora)
-    # https://github.com/annulen/webkit/issues/573
-    patch('qt5-webkit-null-pointer-dereference.patch', level=1)
-    # Fix layout issues in trojita
-    # https://github.com/annulen/webkit/issues/511
-    patch('qt5-webkit-trojita-1.patch', level=1)
-    patch('qt5-webkit-trojita-2.patch', level=1)
 
     # fix detection of python2
     if islinux:
@@ -34,6 +27,8 @@ def main(args):
     cmake_build(
         PORT='Qt', ENABLE_TOOLS='OFF', ENABLE_WEBKIT2='OFF',
         CMAKE_PREFIX_PATH='{0};{0}/qt'.format(PREFIX),
+        # No video
+        ENABLE_VIDEO='OFF',
         # gstreamer is needed for audio/video on Linux
         USE_GSTREAMER='OFF',
         # libhyphen is needed for automatic hyphenation on Linux
