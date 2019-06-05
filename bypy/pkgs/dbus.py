dbec5f5423e6cbfe1843949c3a89bf3895664259
Kovid Goyal <kovid@kovidgoyal.net>
2019-06-05 17:31:59 +0530

    Install proper pkg-config files for dbus

--- bypy/pkgs/dbus.py
+++ bypy/pkgs/dbus.py
@@ -5,19 +5,24 @@
 import os
 import shutil
 
-from bypy.constants import build_dir, MAKEOPTS, PREFIX
-from bypy.utils import run, install_binaries, copy_headers, replace_in_file
+from bypy.constants import MAKEOPTS, PREFIX, build_dir
+from bypy.utils import replace_in_file, run, walk
 
 
 def main(args):
     # dbus chooses where to look for config files based on values fed to
     # ./configure, so we cannot configure to install it to prefix
-    run('./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking --disable-static'
-        ' --disable-doxygen-docs --disable-xml-docs --disable-systemd --without-systemdsystemunitdir'
-        ' --with-console-auth-dir=/run/console/ --disable-tests')
+    run('./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var'
+        ' --disable-dependency-tracking --disable-static'
+        ' --disable-doxygen-docs --disable-xml-docs --disable-systemd'
+        ' --without-systemdsystemunitdir'
+        ' --with-console-auth-dir=/run/console/ --disable-tests --without-x')
     run('make ' + MAKEOPTS)
-    install_binaries('dbus/.libs/libdbus*.so*')
-    os.makedirs(build_dir() + '/lib/pkgconfig')
-    replace_in_file('dbus-1.pc', 'prefix=${original_prefix}', f'prefix={PREFIX}')
-    shutil.copy2('dbus-1.pc', build_dir() + '/lib/pkgconfig')
-    copy_headers('dbus/*.h', destdir='include/dbus-1.0/dbus')
+    run(f'make install', env={'DESTDIR': build_dir()})
+    for x in ('include', 'lib'):
+        os.rename(
+            os.path.join(build_dir(), 'usr', x), os.path.join(build_dir(), x))
+    shutil.rmtree(os.path.join(build_dir(), 'usr'))
+    for path in walk(build_dir()):
+        if path.endswith('.pc'):
+            replace_in_file(path, 'prefix=/usr', f'prefix={PREFIX}')
