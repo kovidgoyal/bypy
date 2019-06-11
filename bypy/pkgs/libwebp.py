2464275b011cacd60099d4ff35cfa5264be1a828
Kovid Goyal <kovid@kovidgoyal.net>
2019-06-11 10:16:18 +0530

    Get libwebp building on windows

--- bypy/pkgs/libwebp.py
+++ bypy/pkgs/libwebp.py
@@ -2,14 +2,14 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from bypy.constants import iswindows
+from bypy.constants import iswindows, NMAKE
 from bypy.utils import simple_build, run, install_binaries, copy_headers
 
 
 def main(args):
     if iswindows:
         run(
-            'nmake /f Makefile.vc CFG=release-dynamic'
+            f'"{NMAKE}" /f Makefile.vc CFG=release-dynamic'
             ' RTLIBCFG=dynamic OBJDIR=output')
         install_binaries('output/release-dynamic/*/bin/*.dll', 'bin')
         install_binaries('output/release-dynamic/*/lib/*.lib', 'lib')
