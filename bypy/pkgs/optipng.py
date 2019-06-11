42ecfc68d1c70ab554e962e42a93c6daf77510cb
Kovid Goyal <kovid@kovidgoyal.net>
2019-06-11 10:50:12 +0530

    Get optipng building on windows

--- bypy/pkgs/optipng.py
+++ bypy/pkgs/optipng.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
-
-from bypy.constants import iswindows
+from bypy.constants import iswindows, NMAKE
 from bypy.utils import simple_build, run, install_binaries
 
 
 def main(args):
     if iswindows:
-        run('nmake -f build\\visualc.mk')
-        install_binaries('src\\optipng\optipng.exe', 'bin', fname_map=lambda x: 'optipng-calibre.exe')
+        run(f'"{NMAKE}" -f build\\visualc.mk')
+        install_binaries('src\\optipng\\optipng.exe', 'bin',
+                         fname_map=lambda x: 'optipng-calibre.exe')
     else:
         simple_build('-with-system-libs')
