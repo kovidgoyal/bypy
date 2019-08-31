5f19c26f23a327fbb76441c2cdff65676cf48e8a
Kovid Goyal <kovid@kovidgoyal.net>
2019-08-31 09:15:52 +0530

    Get gnuwin32 building again

--- bypy/pkgs/gnuwin32.py
+++ bypy/pkgs/gnuwin32.py
@@ -2,6 +2,7 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
+import glob
 import os
 
 from bypy.constants import build_dir
@@ -11,4 +12,6 @@ def main(args):
     os.chdir('..')
     dest = os.path.join(build_dir(), 'private')
     os.makedirs(dest)
-    os.rename('gnuwin32', os.path.join(dest, 'gnuwin32'))
+    srcdir = glob.glob('gnuwin32.zip-*')[0]
+    os.rename(srcdir, os.path.join(dest, 'gnuwin32'))
+    os.mkdir(srcdir)
