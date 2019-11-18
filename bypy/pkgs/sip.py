bbef7a6f2717267b5553c2a48bc5fab884ba648b
Kovid Goyal <kovid@kovidgoyal.net>
2019-11-18 07:49:26 +0530

    Make the sip build python version independent

--- bypy/pkgs/sip.py
+++ bypy/pkgs/sip.py
@@ -5,7 +5,7 @@
 import os
 
 from bypy.constants import (MAKEOPTS, NMAKE, PREFIX, PYTHON, build_dir,
-                            ismacos, iswindows)
+                            ismacos, iswindows, python_major_minor_version)
 from bypy.utils import replace_in_file, run
 
 
@@ -19,8 +19,9 @@ def main(args):
         PYTHON, 'configure.py', '--no-pyi',
         '--sip-module=PyQt5.sip',
         '--bindir=%s/bin' % build_dir()]
-    sp = 'Lib' if iswindows else 'lib/python2.7'
-    inc = 'include' if iswindows else 'include/python2.7'
+    pyver = python_major_minor_version()
+    sp = 'Lib' if iswindows else 'lib/python{}.{}'.format(*pyver)
+    inc = 'include' if iswindows else 'include/python{}.{}'.format(*pyver)
     cmd += ['--destdir=%s/%s/site-packages' % (b, sp),
             '--sipdir=%s/share/sip' % b,
             '--incdir=%s/%s' % (b, inc)]
@@ -34,5 +35,5 @@ def main(args):
     if iswindows:
         q = q.replace(os.sep, os.sep + os.sep)
         r = r.replace(os.sep, os.sep + os.sep)
-    p = 'Lib' if iswindows else 'lib/python2.7'
+    p = 'Lib' if iswindows else 'lib/python{}.{}'.format(*pyver)
     replace_in_file(os.path.join(b, p, 'site-packages/sipconfig.py'), q, r)
