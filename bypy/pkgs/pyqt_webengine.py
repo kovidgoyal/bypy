775a9a74eb663f54d1bdf86db8b411136d4a12c4
Kovid Goyal <kovid@kovidgoyal.net>
2019-11-24 13:19:56 +0530

    Get pyqt webengine building again on windows, no idea when and why it broke, but monkeypatch the makefiles to fix

--- bypy/pkgs/pyqt_webengine.py
+++ bypy/pkgs/pyqt_webengine.py
@@ -2,15 +2,22 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
+import glob
 import os
+import re
 
-from bypy.constants import PREFIX, PYTHON
+from bypy.constants import PREFIX, PYTHON, iswindows
 from bypy.pkgs.pyqt import run_build, run_configure
-from bypy.utils import run
+from bypy.utils import replace_in_file, run
 
 
 def main(args):
     run_configure(for_webengine=True)
+    if iswindows:
+        libs = 'WebEngine|Quick|WebChannel|Qml|Positioning'
+        pat = r'(C:\S+?)bin(.Qt5(?:{})\S*?.lib)'.format(libs)
+        for fname in glob.glob('*/Makefile.Release'):
+            replace_in_file(fname, re.compile(pat), r'\1lib\2')
     run_build()
 
 
