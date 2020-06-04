5bc841d9a1dab9098d6cdb13b2d744248e121a62
Kovid Goyal <kovid@kovidgoyal.net>
2020-06-04 17:26:36 +0530

    Fix wayland_protocols pkgconfig file

--- bypy/pkgs/wayland-protocols.py
+++ bypy/pkgs/wayland-protocols.py
@@ -3,18 +3,14 @@
 # License: GPLv3 Copyright: 2020, Kovid Goyal <kovid at kovidgoyal.net>
 
 import os
-import re
-from glob import glob
 
-from ..constants import PREFIX, build_dir
-from ..utils import replace_in_file, simple_build
+from ..constants import build_dir
+from ..utils import simple_build
 
 
 def main(args):
     simple_build()
     pcdir = os.path.join(build_dir(), 'lib/pkgconfig')
     os.makedirs(pcdir)
-    for x in glob(os.path.join(build_dir(), 'share/pkgconfig/*.pc')):
-        replace_in_file(x, re.compile(r'^prefix=.+$', re.M),
-                        'prefix=%s' % PREFIX)
-        os.rename(x, x.replace('/share/', '/lib/'))
+    pc = os.path.join(build_dir(), 'share/pkgconfig/wayland-protocols.pc')
+    os.rename(pc, pc.replace('/share/', '/lib'))
