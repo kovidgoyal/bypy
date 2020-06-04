282e73b85cc2ff4a770bb191de238cfa0a4706db
Kovid Goyal <kovid@kovidgoyal.net>
2020-06-04 11:34:03 +0530

    More robust meson installation

--- bypy/linux.py
+++ bypy/linux.py
@@ -151,11 +151,13 @@ def _build_container(url=DEFAULT_BASE_IMAGE):
         'apt-get update',
         'apt-get install -y build-essential cmake software-properties-common'
         ' nasm chrpath zsh git uuid-dev libmount-dev'
-        ' dh-autoreconf ninja-build python3-pip',
-        'pip3 install meson',
+        ' dh-autoreconf ninja-build',
         'add-apt-repository ppa:deadsnakes/ppa -y',
         'apt-get update',
         'apt-get install -y python3.7',
+        'curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py',
+        'python3.7 get-pip.py',
+        'python3.7 -m pip install meson',
         deps_cmd,
         # Cleanup
         'apt-get clean',
