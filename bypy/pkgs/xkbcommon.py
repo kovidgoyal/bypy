957a65919aad917c04f65db008a2a2563568d352
Kovid Goyal <kovid@kovidgoyal.net>
2020-06-04 11:51:17 +0530

    Code to build libxkbcommon

---
+++ bypy/pkgs/xkbcommon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+from bypy.utils import meson_build
+
+
+def main(args):
+    meson_build(**{
+        'enable-wayland': 'false',
+        'xkb-config-root': '/usr/share/X11/xkb',
+        'x=locale-root': '/usr/share/X11/locale',
+        'enable-docs': 'false',
+    })
