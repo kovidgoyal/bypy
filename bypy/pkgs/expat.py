275223d5cfb87f8f467caad30df5a54e93a19b2d
Kovid Goyal <kovid@kovidgoyal.net>
2020-08-24 21:10:00 +0530

    expat CMakeLists changed the library names

--- bypy/pkgs/expat.py
+++ bypy/pkgs/expat.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
 from bypy.constants import iswindows
 from bypy.utils import windows_cmake_build
 
@@ -11,6 +9,6 @@ from bypy.utils import windows_cmake_build
 if iswindows:
     def main(args):
         windows_cmake_build(
-            BUILD_tools='OFF', BUILD_examples='OFF', BUILD_tests='OFF', BUILD_doc='OFF',
-            binaries='expat.dll', libraries='expat.lib', headers='../lib/expat.h ../lib/expat_external.h'
+            binaries='libexpat.dll', libraries='libexpat.lib',
+            headers='../lib/expat.h ../lib/expat_external.h'
         )
