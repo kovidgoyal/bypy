d6bbfba888502c7bccdc30f8f7442810a06e4a48
Kovid Goyal <kovid@kovidgoyal.net>
2020-08-25 08:07:19 +0530

    Disable accessibility in the Linux builds
    It causes slowdowns/crashes and there is no longer a way to disable it
    at runtime

--- bypy/pkgs/qt_base.py
+++ bypy/pkgs/qt_base.py
@@ -58,8 +58,10 @@ def main(args):
     if islinux:
         # Gold linker is needed for Qt 5.13.0 because of
         # https://bugreports.qt.io/browse/QTBUG-76196
+        # Accessibility is disabled because it causes crashes
+        # https://bugs.launchpad.net/bugs/1892646
         conf += (' -bundled-xcb-xinput -xcb -glib -openssl -qt-pcre'
-                 ' -xkbcommon -libinput -linker gold')
+                 ' -xkbcommon -libinput -linker gold -no-accessibility')
     elif ismacos:
         conf += ' -no-pkg-config -framework -no-openssl -securetransport'
         ' -no-freetype -no-fontconfig '
