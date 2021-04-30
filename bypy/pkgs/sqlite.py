e906c562ef6886b4a2a92061410c909f5c42beca
Kovid Goyal <kovid@kovidgoyal.net>
2021-04-30 23:37:43 +0530

    Cleanup sqlite build script

--- bypy/pkgs/sqlite.py
+++ bypy/pkgs/sqlite.py
@@ -2,11 +2,8 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
 from bypy.constants import CFLAGS, UNIVERSAL_ARCHES, ismacos, iswindows
-from bypy.utils import ModifiedEnv, copy_headers, simple_build
+from bypy.utils import copy_headers, simple_build
 
 
 def main(args):
@@ -15,14 +12,12 @@ def main(args):
         # downloads its own version locked sqlite. We just install the headers
         copy_headers('sqlite3*.h')
         return
-    cflags = CFLAGS
-    env = {}
+    configure = '--disable-dependency-tracking --disable-static'.split()
     if ismacos:
-        cflags += ' -O2 -DSQLITE_ENABLE_LOCKING_STYLE'
+        configure.append(f'CFLAGS={CFLAGS} -O2 -DSQLITE_ENABLE_LOCKING_STYLE')
         if len(UNIVERSAL_ARCHES) > 1:
-            env['CC'] = 'clang ' + ' '.join(
-                f'-arch {x}' for x in UNIVERSAL_ARCHES)
-            env['CPP'] = 'clang -E'
-    env['CFLAGS'] = cflags
-    with ModifiedEnv(**env):
-        simple_build('--disable-dependency-tracking --disable-static')
+            configure.append(
+                'CC=' + 'clang ' + ' '.join(
+                    f'-arch {x}' for x in UNIVERSAL_ARCHES))
+            configure.append('CPP=clang -E')
+    simple_build(configure)
