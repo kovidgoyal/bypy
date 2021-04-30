6b845dbb78be3893937ca2117de5f71d6315e8d9
Kovid Goyal <kovid@kovidgoyal.net>
2021-04-30 23:08:43 +0530

    harfbuzz universal binary

--- bypy/pkgs/harfbuzz.py
+++ bypy/pkgs/harfbuzz.py
@@ -2,15 +2,26 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2020, Kovid Goyal <kovid at kovidgoyal.net>
 
-from ..constants import ismacos
+from ..constants import ismacos, current_build_arch, UNIVERSAL_ARCHES
 from ..utils import simple_build
+
+
+needs_lipo = True
 
 
 def main(args):
     ft = 'no' if ismacos else 'yes'
     ct = 'yes' if ismacos else 'no'
-    simple_build(
+    configure = (
         '--disable-dependency-tracking --disable-static --with-glib=no'
-        ' --with-freetype={} --with-gobject=no --with-cairo=no'
-        ' --with-fontconfig=no --with-icu=no --with-coretext={}'
-        .format(ft, ct))
+        f' --with-freetype={ft} --with-gobject=no --with-cairo=no'
+        f' --with-fontconfig=no --with-icu=no --with-coretext={ct}'
+    ).split()
+    if UNIVERSAL_ARCHES and 'arm' in current_build_arch():
+        configure += [
+            '--build=x86_64-apple-darwin', '--host=aarch64-apple-darwin',
+            f'CFLAGS=-arch {current_build_arch()}',
+            f'CXXFLAGS=-arch {current_build_arch()}',
+        ]
+
+    simple_build(configure)
