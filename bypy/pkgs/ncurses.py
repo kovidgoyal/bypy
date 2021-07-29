6379b3933146c4c4b4617f8e1dd7272753c7c2e6
Kovid Goyal <kovid@kovidgoyal.net>
2021-07-29 12:11:30 +0530

    Ensure ncurses looks in common places for terminfo databases

--- bypy/pkgs/ncurses.py
+++ bypy/pkgs/ncurses.py
@@ -5,13 +5,28 @@
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
-from bypy.constants import PKG_CONFIG_PATH
-from bypy.utils import simple_build
+from bypy.constants import PKG_CONFIG_PATH, build_dir
+from bypy.utils import simple_build, ModifiedEnv
 
 
 def main(args):
-    simple_build('--with-shared --without-debug --without-ada --enable-widec --with-normal --enable-pc-files --with-pkg-config-libdir=%s' % PKG_CONFIG_PATH)
+    # make install tries to write to $HOME/.terminfo
+    with ModifiedEnv(HOME=build_dir()):
+        simple_build(
+            '--with-shared --without-debug --without-ada --enable-widec'
+            ' --with-normal --enable-pc-files'
+            f' --with-pkg-config-libdir={PKG_CONFIG_PATH}'
+            # without the following ncurses will look in the BUILD_DIR
+            # for terminfo files even on target systems. Instead use
+            # a bunch of common locations.
+            ' --with-terminfo-dirs=/usr/share/terminfo:/etc/terminfo:'
+            '/lib/terminfo:/usr/lib/terminfo'
+            ' --with-default-terminfo-dir=/usr/share/terminfo',
+        )
 
 
 def filter_pkg(parts):
-    return 'terminfo' in parts or 'tabset' in parts or 'bin' in parts
+    return (
+        'terminfo' in parts or 'tabset' in parts or 'bin' in parts or
+        '.terminfo' in parts
+    )
