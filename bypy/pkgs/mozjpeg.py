6daa6ae277a407b9117fcba842659449ab886eb0
Kovid Goyal <kovid@kovidgoyal.net>
2021-11-25 15:27:38 +0530

    Move to building mozjpeg with cmake on unix as well

--- bypy/pkgs/mozjpeg.py
+++ bypy/pkgs/mozjpeg.py
@@ -3,11 +3,10 @@
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
 import os
-import re
 
-from bypy.constants import BIN, build_dir, ismacos
-from bypy.utils import (ModifiedEnv, install_binaries, iswindows, run,
-                        simple_build, windows_cmake_build, replace_in_file)
+from bypy.constants import BIN, ismacos
+from bypy.utils import (cmake_build, install_binaries, iswindows,
+                        windows_cmake_build)
 
 
 def main(args):
@@ -23,18 +22,4 @@ def main(args):
         env = {}
         if ismacos:
             env['PATH'] = BIN + os.pathsep + os.environ['PATH']
-            env['LIBTOOLIZE'] = 'glibtoolize'
-            env['LIBTOOL'] = 'glibtool'
-        with ModifiedEnv(**env):
-            run('autoreconf -fiv')
-        conf = ('--disable-dependency-tracking --disable-shared --with-jpeg8'
-                ' --without-turbojpeg')
-        env = {}
-        if ismacos:
-            conf += f' --host x86_64-apple-darwin NASM={BIN}/nasm'
-            replace_in_file('configure', re.compile(
-                br'^PKG_CHECK_MODULES.libpng.+?\bfi\b', re.M | re.DOTALL),
-                'HAVE_LIBPNG=1\nHAVE_LIBPNG_TRUE="#"\n')
-        simple_build(conf,
-                     override_prefix=os.path.join(build_dir(), 'private',
-                                                  'mozjpeg'))
+        cmake_build(env=env)
