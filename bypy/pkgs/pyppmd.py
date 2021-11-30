c83056e2a3835f348d21c06bf28c1a85925f031a
Kovid Goyal <kovid@kovidgoyal.net>
2021-11-30 11:58:00 +0530

    Fix build of pyppmd
    It's missing an __init__.py file

---
+++ bypy/pkgs/pyppmd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+import os
+
+from bypy.utils import python_build, python_install, walk, build_dir
+
+
+def main(args):
+    python_build()
+    for f in walk(build_dir()):
+        if os.path.basename(f) == 'c_ppmd.py':
+            q = os.path.join(os.path.dirname(f), '__init__.py')
+            if not os.path.exists(q):
+                open(q, 'w').close()
+    python_install()
