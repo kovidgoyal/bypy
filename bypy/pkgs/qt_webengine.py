e6c026b47f4ed69e65bc0d913a7fbea8be8b86c5
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-01 08:07:28 +0530

    webengine: use system ICU

--- bypy/pkgs/qt_webengine.py
+++ bypy/pkgs/qt_webengine.py
@@ -13,19 +13,9 @@ def main(args):
     if islinux:
         # see https://bugs.launchpad.net/calibre/+bug/1939958
         apply_patch('crbug1213452.diff', level=1)
-        # workaround for bug in build system, not adding include path for
-        # libjpeg when building iccjpeg, and mjpeg_decoder
-        # jpeg_files = list(glob.glob(f'{PREFIX}/include/*jpeg*.h'))
-        # jpeg_files += [
-        #     f'{PREFIX}/include/{x}.h'
-        #     for x in 'jerror jconfig jmorecfg'.split()
-        # ]
-        # for header in jpeg_files:
-        #     os.symlink(
-        #         header,
-        #         os.path.join('src/3rdparty/chromium',
-        #                      os.path.basename(header)))
-        # conf += ' -webp -webengine-icu'
+        # use system ICU otherwise there is 10MB duplication and we have to
+        # make resources/icudtl.dat available in the application
+        conf += ' -webengine-icu'
 
     if iswindows:
         # broken test for 64-bit ness needs to be disabled
