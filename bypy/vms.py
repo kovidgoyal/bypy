1d406f3b33e33fd094b01213fcd2d770533b88e7
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-12 13:16:21 +0530

    Fix bootstrapping of VM

--- bypy/vms.py
+++ bypy/vms.py
@@ -130,8 +130,9 @@ def to_vm(rsync, initial_cmd, sources_dir, pkg_dir, prefix='/', name='sw'):
         run_sync_jobs(to_vm_calls)
         print(f'Mirroring took {time.monotonic() - start:.1f} seconds', flush=True)
     elif cp.returncode == 1:
+        # initial creation when bypy is not present or outdated in the VM
         print('Running initial cmd to sync data to VM failed', initial_cmd, file=sys.stderr, flush=True)
-        raise SystemExit(1)
+        rsync.run_via_ssh('mkdir', '-p', *dirs_to_ensure)
     elif cp.returncode == 13:
         print('There is an existing job running, reconnecting to that job...', flush=True)
 
