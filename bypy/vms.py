a3945a6cab5bb0d9d4f70f1f15cb579e5fde9413
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-12 13:17:11 +0530

    ...

--- bypy/vms.py
+++ bypy/vms.py
@@ -133,6 +133,8 @@ def to_vm(rsync, initial_cmd, sources_dir, pkg_dir, prefix='/', name='sw'):
         # initial creation when bypy is not present or outdated in the VM
         print('Running initial cmd to sync data to VM failed', initial_cmd, file=sys.stderr, flush=True)
         rsync.run_via_ssh('mkdir', '-p', *dirs_to_ensure)
+        run_sync_jobs(to_vm_calls)
+        print(f'Mirroring took {time.monotonic() - start:.1f} seconds', flush=True)
     elif cp.returncode == 13:
         print('There is an existing job running, reconnecting to that job...', flush=True)
 
