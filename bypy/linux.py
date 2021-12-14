aa2abd858f8ec2e82d31a9496dede60c043b9458
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-14 11:28:07 +0530

    DRYer

--- bypy/linux.py
+++ bypy/linux.py
@@ -18,24 +18,25 @@ def main(args=tuple(sys.argv)):
     chroot = Chroot(arch)
     if not chroot.single_instance():
         raise SystemExit('Another instance of the Linux container is running')
-    if len(args) > 1:
-        if args[1] == 'shutdown':
-            shutdown(vm)
-            return
-        if args[1] == 'vm':
-            chroot.build_vm()
-            return
-
-    chroot.ensure_vm_is_built(vm)
-
-    port = wait_for_ssh(vm)
-    rsync = Rsync(vm, port)
     output_dir = os.path.join(base_dir(), 'b', 'linux', arch, 'dist')
     pkg_dir = os.path.join(base_dir(), 'b', 'linux', arch, 'pkg')
     sources_dir = os.path.join(base_dir(), 'b', 'sources-cache')
     os.makedirs(sources_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(pkg_dir, exist_ok=True)
+    special_arg = args[1] if len(args) > 1 else ''
+
+    if special_arg == 'shutdown':
+        shutdown(vm)
+        return
+    if special_arg == 'vm':
+        chroot.build_vm()
+        return
+
+    chroot.ensure_vm_is_built(vm)
+
+    port = wait_for_ssh(vm)
+    rsync = Rsync(vm, port)
 
     cmd = ['python3', os.path.join('/', 'bypy'), 'main']
     rsync.main(sources_dir, pkg_dir, output_dir, cmd, list(args))
