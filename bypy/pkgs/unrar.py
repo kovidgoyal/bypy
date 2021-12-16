27799a37813fd92d0c1a838a5f84d08ab31f6867
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-16 15:33:28 +0530

    univ unrar

--- bypy/pkgs/unrar.py
+++ bypy/pkgs/unrar.py
@@ -3,8 +3,10 @@
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
 from bypy.constants import ismacos, iswindows
-from bypy.utils import (copy_headers, install_binaries, msbuild,
-                        replace_in_file, run)
+from bypy.utils import (
+    copy_headers, current_build_arch, install_binaries,
+    is_arm_half_of_lipo_build, msbuild, replace_in_file, run
+)
 
 needs_lipo = True
 
@@ -22,8 +24,13 @@ def main(args):
         # from bypy.utils import run_shell
         # run_shell()
     else:
+        flags = '-fPIC'
         if ismacos:
             replace_in_file('makefile', 'libunrar.so', 'libunrar.dylib')
-        run('make -j4 lib CXXFLAGS=-fPIC')
+            if is_arm_half_of_lipo_build():
+                flags += f' -arch {current_build_arch()}'
+        replace_in_file('makefile', 'CXXFLAGS=', f'CXXFLAGS={flags} ')
+        replace_in_file('makefile', 'LDFLAGS=', f'LDFLAGS=-arch {current_build_arch()} ')
+        run('make -j4 lib')
         install_binaries('libunrar.' + ('dylib' if ismacos else 'so'), 'lib')
     copy_headers('*.hpp', destdir='include/unrar')
