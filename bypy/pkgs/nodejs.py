dbc7c2db4a2384a019e6db5e2e491af1a267226e
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-17 19:48:18 +0530

    Script to build nodejs

---
+++ bypy/pkgs/nodejs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2021, Kovid Goyal <kovid at kovidgoyal.net>
+
+from ..utils import simple_build
+
+
+allow_non_universal = True
+
+
+def main(args):
+    simple_build()
