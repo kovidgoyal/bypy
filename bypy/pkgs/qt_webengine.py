657554331e1c5a20ef03a0d0a18b8bb093444fc5
Kovid Goyal <kovid@kovidgoyal.net>
2023-01-15 08:46:00 +0530

    The webengine windows N patch was merged upstream

--- bypy/pkgs/qt_webengine.py
+++ bypy/pkgs/qt_webengine.py
@@ -4,8 +4,8 @@
 
 
 import os
-from bypy.constants import islinux, iswindows
-from bypy.utils import qt_build, total_physical_ram, apply_patch
+from bypy.constants import islinux
+from bypy.utils import qt_build, total_physical_ram
 
 
 def main(args):
@@ -17,9 +17,6 @@ def main(args):
         # use system ICU otherwise there is 10MB duplication and we have to
         # make resources/icudtl.dat available in the application
         conf += ' -webengine-icu'
-    elif iswindows:
-        # https://code.qt.io/cgit/qt/qtwebengine.git/commit/?id=74163a6511278fa8273ca931ffdf9b9b3a8daae6
-        apply_patch('qt6-webengine-windows-n.patch', level=1)
 
     qt_build(conf, for_webengine=True)
 
