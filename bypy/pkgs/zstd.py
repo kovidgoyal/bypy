46926c6b43f567ae5c3a0bc4dc270ed843ede5ca
xcffl <xcffl@protonmail.com>
2023-02-20 13:29:20 +0800

    fix: zstd is not installed to the right location

--- bypy/pkgs/zstd.py
+++ bypy/pkgs/zstd.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from bypy.constants import NMAKE, UNIVERSAL_ARCHES, iswindows
+from bypy.constants import NMAKE, UNIVERSAL_ARCHES, build_dir, iswindows, MAKEOPTS
 from bypy.utils import (copy_headers, install_binaries,
                         replace_in_file, run, simple_build)
 
@@ -13,13 +13,5 @@ if iswindows:
         copy_headers('include/zstd.h')
 else:
     def main(args):
-        configure_args = []
-        if len(UNIVERSAL_ARCHES) > 1:
-            replace_in_file(
-                'configure',
-                'CFLAGS="${CFLAGS} ${ARCHS}"',
-                'CFLAGS="${CFLAGS} ${ARCHS}"\n  SFLAGS="${SFLAGS} ${ARCHS}"'
-            )
-            archs = ' '.join(f'-arch {x}' for x in UNIVERSAL_ARCHES)
-            configure_args.append(f'--archs={archs}')
-        simple_build(configure_args)
+        run('make', MAKEOPTS)
+        run('make', 'DESTDIR={}'.format(build_dir()), 'install')
