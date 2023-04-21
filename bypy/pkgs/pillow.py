6cdc2ca1ee77227d0c9018f5b08ea016f1238927
Kovid Goyal <kovid@kovidgoyal.net>
2023-04-21 12:34:12 +0530

    Enable webp support in Pillow on windows

--- bypy/pkgs/pillow.py
+++ bypy/pkgs/pillow.py
@@ -2,10 +2,7 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
 import os
-import re
 
 from bypy.constants import iswindows, PREFIX
 from bypy.utils import replace_in_file, python_build, python_install
@@ -13,14 +10,24 @@ from bypy.utils import replace_in_file, python_build, python_install
 
 if iswindows:
     def main(args):
-        root = (
-            os.path.join(PREFIX, 'lib').replace('\\', '/'),
-            os.path.join(PREFIX, 'include').replace('\\', '/')
-        )
-        replace_in_file(
-            'setup.py',
-            re.compile(r'^(JPEG|ZLIB|FREETYPE)_ROOT = None', re.M),
-            fr'\1_ROOT = {root}')
+        incdir = os.path.join(PREFIX, 'include')
+        libdir = os.path.join(PREFIX, 'lib')
+        with open('setup.cfg', 'a') as s:
+            print(f'''
+[build_ext]
+include_dirs = {incdir}
+library_dirs = {libdir}
+enable_zlib = True
+enable_jpeg = True
+enable_webp = True
+enable_webpmux = True
+enable_freetype = True
+''', file=s)
+
+        replace_in_file('setup.py', 'DEBUG = False', 'DEBUG = True')
+        replace_in_file('setup.py', '"libwebp"', '"libwebp_dll"')
+        replace_in_file('setup.py', '"libwebpmux"', '"libwebpmux_dll"')
+        replace_in_file('setup.py', '"libwebpdemux"', '"libwebpdemux_dll"')
         # dont link against static zlib
         replace_in_file(
             'setup.py', 'feature.zlib = "zlib"', 'feature.zlib = "zdll"')
