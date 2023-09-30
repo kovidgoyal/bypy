0e6703560aa5850f42997f72fd28a7b475da4b99
Kovid Goyal <kovid@kovidgoyal.net>
2023-09-30 11:17:09 +0530

    Fix xxhash build

---
+++ bypy/pkgs/xxhash.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2023, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+from bypy.utils import python_build, python_install
+
+def main(args):
+    python_build(ignore_dependencies=True)  # it states it depends on setuptools_scm but doesnt actually when biulding published sources
+    python_install()
