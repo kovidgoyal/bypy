676fa3b7487b0b637f42580d854be69b5c70244c
Kovid Goyal <kovid@kovidgoyal.net>
2023-10-09 09:01:18 +0530

    Use the latest available Visual Studio

--- bypy/vcvars.py
+++ bypy/vcvars.py
@@ -4,13 +4,10 @@
 
 import ctypes.wintypes
 import os
+import re
 import subprocess
 from functools import lru_cache
 from glob import glob
-
-# Visual Studio 2022
-VS_VERSION = '17.0'
-COMN_TOOLS_VERSION = '170'
 
 CSIDL_PROGRAM_FILES = 38
 CSIDL_PROGRAM_FILESX86 = 42
@@ -41,10 +38,10 @@ def get_output(*cmd):
 
 
 @lru_cache()
-def find_visual_studio(version=VS_VERSION):
+def find_visual_studio():
     path = get_output(
         find_vswhere(),
-        "-version", version,
+        "-latest",
         "-requires",
         "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
         "-property",
@@ -56,10 +53,10 @@ def find_visual_studio(version=VS_VERSION):
 
 
 @lru_cache()
-def find_msbuild(version=VS_VERSION):
+def find_msbuild():
     base_path = get_output(
         find_vswhere(),
-        "-version", version,
+        "-latest",
         "-requires", "Microsoft.Component.MSBuild",
         "-property", 'installationPath'
     ).strip()
@@ -121,6 +118,16 @@ def query_vcvarsall(is64bit=True):
     plat = 'amd64' if is64bit else 'amd64_x86'
     vcvarsall = find_vcvarsall()
     env = query_process('"%s" %s & set' % (vcvarsall, plat), is64bit)
+    pat = re.compile('vs(\d+)comntools', re.I)
+
+    comn_tools = {}
+
+    for k in env:
+        m = pat.match(k)
+        if m is not None:
+            comn_tools[k] = int(m.group(1))
+    comntools = sorted(comn_tools, key=comn_tools.__getitem__)[-1]
+
 
     def g(k):
         try:
@@ -131,8 +138,7 @@ def query_vcvarsall(is64bit=True):
     return {
         k: g(k)
         for k in (
-            'PATH LIB INCLUDE LIBPATH WINDOWSSDKDIR'
-            f' VS{COMN_TOOLS_VERSION}COMNTOOLS PLATFORM'
+            f'PATH LIB INCLUDE LIBPATH WINDOWSSDKDIR PLATFORM {comntools}'
             ' UCRTVERSION UNIVERSALCRTSDKDIR VCTOOLSVERSION WINDOWSSDKDIR'
             ' WINDOWSSDKVERSION WINDOWSSDKVERBINPATH WINDOWSSDKBINPATH VCINSTALLDIR VSINSTALLDIR'
             ' VISUALSTUDIOVERSION VSCMD_ARG_HOST_ARCH VSCMD_ARG_TGT_ARCH'
