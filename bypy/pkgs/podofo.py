dc2b7e297e201cb8b019b9c017952af927a38dec
Kovid Goyal <kovid@kovidgoyal.net>
2023-10-10 18:45:06 +0530

    Simplify podofo build on windows

--- bypy/pkgs/podofo.py
+++ bypy/pkgs/podofo.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from bypy.constants import PREFIX, iswindows
+from bypy.constants import iswindows
 from bypy.utils import (
     cmake_build, copy_headers, install_binaries, walk, windows_cmake_build, replace_in_file
 )
@@ -13,26 +13,11 @@ def main(args):
     # want libpodofo to link against the system libtiff
     replace_in_file('CMakeLists.txt', 'if(TIFF_FOUND)', 'if(TIFF_FOUND_DISABLED)')
 
-    if iswindows:
-        windows_cmake_build(
-            WANT_LIB64='FALSE',
-            PODOFO_BUILD_STATIC='False',
-            FREETYPE_INCLUDE_DIR=f"{PREFIX}/include/freetype2",
-            nmake_target='podofo_shared'
-        )
-        copy_headers('build/src/podofo/podofo_config.h', 'include/podofo')
-        copy_headers('src/podofo/*', 'include/podofo')
-        for f in walk():
-            if f.endswith('.dll'):
-                install_binaries(f, 'bin')
-            elif f.endswith('.lib'):
-                install_binaries(f, 'lib')
-    else:
-        cmake_build(
-            make_args='podofo_shared',
-            PODOFO_BUILD_LIB_ONLY='TRUE',
-            PODOFO_BUILD_STATIC='FALSE',
-        )
+    cmake_build(
+        make_args='podofo_shared',
+        PODOFO_BUILD_LIB_ONLY='TRUE',
+        PODOFO_BUILD_STATIC='FALSE',
+    )
 
 
 def modify_excludes(excludes):
