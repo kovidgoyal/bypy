5da9b9e1b3777c866736fb71f88b916edecd4e01
Kovid Goyal <kovid@kovidgoyal.net>
2023-10-10 13:40:50 +0530

    Use cmake to build libxml on windows as well

--- bypy/pkgs/libxml2.py
+++ bypy/pkgs/libxml2.py
@@ -3,43 +3,24 @@
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
 
-import os
 import re
 
-from bypy.constants import LIBDIR, NMAKE, PREFIX, build_dir, ismacos, iswindows
-from bypy.utils import (
-    install_binaries, install_tree, replace_in_file, run, simple_build, walk, cmake_build
-)
+from bypy.constants import PREFIX, build_dir, ismacos, iswindows
+from bypy.utils import cmake_build, replace_in_file, simple_build, walk
+
 
 def main(args):
-    if iswindows:
-        run(*('cscript.exe configure.js include={0}/include'
-            ' lib={0}/lib prefix={0} zlib=yes iconv=yes'.format(
-                PREFIX.replace(os.sep, '/')).split()), cwd='win32')
-        replace_in_file('win32/Makefile.msvc', 'iconv.lib', 'libiconv.lib')
-        run(f'"{NMAKE}" /f Makefile.msvc', cwd='win32')
-        install_tree('include/libxml', 'include/libxml2')
-        for f in walk('.'):
-            if f.endswith('.dll'):
-                install_binaries(f, 'bin')
-            elif f.endswith('.lib'):
-                install_binaries(f)
-    elif ismacos:
+    if iswindows or ismacos:
         cmake_build(
             LIBXML2_WITH_ICU='ON', LIBXML2_WITH_PYTHON='OFF', LIBXML2_WITH_TESTS='OFF',
-            LIBXML2_WITH_LZMA='OFF',
+            LIBXML2_WITH_LZMA='OFF', relocate_pkgconfig=not iswindows,
         )
     else:
         # ICU is needed to use libxml2 in qt-webengine
-        env = {}
-        if ismacos:
-            env['ICU_CFLAGS'] = f'-I{PREFIX}/include'
-            env['ICU_LIBS'] = f'-L{LIBDIR} -licuc -licudata'
-            env['ICU_DEFS'] = '-DDUMMY_DEF_FOR_STUPID_CONFIGURE_SCRIPT'
         simple_build(
             '--disable-dependency-tracking --disable-static --enable-shared'
             ' --without-python --without-debug --with-iconv={0} --disable-silent-rules'
-            ' --with-zlib={0} --with-icu'.format(PREFIX), env=env)
+            ' --with-zlib={0} --with-icu'.format(PREFIX))
         for path in walk(build_dir()):
             if path.endswith('/xml2-config'):
                 replace_in_file(
--- bypy/pkgs/libxslt.py
+++ bypy/pkgs/libxslt.py
@@ -3,39 +3,13 @@
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
 
-import os
 
-from bypy.constants import NMAKE, PREFIX, ismacos, iswindows
-from bypy.utils import (
-    cmake_build, install_binaries, install_tree, replace_in_file, run,
-    simple_build, walk
-)
+from bypy.constants import PREFIX, ismacos, iswindows
+from bypy.utils import cmake_build, simple_build
 
 
 def main(args):
-    if iswindows:
-        run(*(
-            'cscript.exe configure.js include={0}/include'
-            ' include={0}/include/libxml2 lib={0}/lib prefix={0}'
-            ' zlib=yes iconv=yes'.format(
-                PREFIX.replace(os.sep, '/')).split()), cwd='win32')
-        for f in walk('.'):
-            bname = os.path.basename(f)
-            if bname.startswith('Makefile'):
-                replace_in_file(f, '/OPT:NOWIN98', '', missing_ok=True)
-                if bname == 'Makefile.msvc':
-                    replace_in_file(f, 'iconv.lib', 'libiconv.lib')
-            # elif bname == 'xsltconfig.h':
-            #     replace_in_file(f, '@WITH_PROFILER@', '1')
-        run(f'"{NMAKE}" /f Makefile.msvc', cwd='win32')
-        install_tree('libxslt', 'include')
-        install_tree('libexslt', 'include')
-        for f in walk('.'):
-            if f.endswith('.dll'):
-                install_binaries(f, 'bin')
-            elif f.endswith('.lib'):
-                install_binaries(f)
-    elif ismacos:
+    if ismacos or iswindows:
         cmake_build(
             LIBXSLT_WITH_PYTHON='OFF', LIBXML2_INCLUDE_DIR=f'{PREFIX}/include',
             relocate_pkgconfig=False
