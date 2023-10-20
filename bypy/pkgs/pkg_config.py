7290a86cb2029fd7657b32005091e8ff4712ecf5
Kovid Goyal <kovid@kovidgoyal.net>
2023-10-20 12:03:06 +0530

    Build universal pkg-config

--- bypy/pkgs/pkg_config.py
+++ bypy/pkgs/pkg_config.py
@@ -2,11 +2,13 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
+from bypy.constants import UNIVERSAL_ARCHES
 from bypy.utils import simple_build
 
 
-allow_non_universal = True
-
-
 def main(args):
-    simple_build('--with-internal-glib --disable-host-tool')
+    suffix = ''
+    if UNIVERSAL_ARCHES:
+        flags = ' '.join(f'-arch {x}' for x in UNIVERSAL_ARCHES)
+        suffix = f' CFLAGS="{flags}" LDFLAGS="{flags}"'
+    simple_build('--with-internal-glib --disable-silent-rules --disable-host-tool' + suffix)
