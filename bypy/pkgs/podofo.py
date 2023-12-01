578c1ccfc8e3b5b071b0207a89521638feb90205
Kovid Goyal <kovid@kovidgoyal.net>
2023-12-01 13:10:32 +0530

    Get podofo building with libxml2 2.12.1

--- bypy/pkgs/podofo.py
+++ bypy/pkgs/podofo.py
@@ -2,16 +2,19 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-from bypy.constants import iswindows
-from bypy.utils import (
-    cmake_build, copy_headers, install_binaries, walk, windows_cmake_build, replace_in_file
-)
+from bypy.utils import cmake_build, replace_in_file
 
 
 def main(args):
     # Linux distros have libtiff but calibre does not bundle libtiff, we dont
     # want libpodofo to link against the system libtiff
     replace_in_file('CMakeLists.txt', 'if(TIFF_FOUND)', 'if(TIFF_FOUND_DISABLED)')
+
+    # Patches to build with newer versions of libxml2
+    # https://github.com/podofo/podofo/commit/d470a0a2ad423042c29db08ba13170f42d8806cf
+    replace_in_file('src/podofo/private/XmlUtils.h', '#include <libxml/tree.h>', '#include <libxml/tree.h>\n#include <libxml/xmlerror.h>')
+    replace_in_file('src/podofo/private/XmlUtils.h', 'xmlErrorPtr error_', 'const xmlError *error_')
+    replace_in_file('src/podofo/main/PdfXMPPacket.cpp', '#include <libxml/xmlsave.h>', '#include <libxml/xmlsave.h>\n#include <libxml/parser.h>')
 
     cmake_build(
         make_args='podofo_shared',
