409434c66dc3492003c7dd05708084196887f7d2
Matthew Wener <125427297+ConstrictM@users.noreply.github.com>
2024-06-10 12:34:38 -0400

    Patch mozjpeg so it Can find the PNG/ZLIB Paths

--- bypy/pkgs/mozjpeg.py
+++ bypy/pkgs/mozjpeg.py
@@ -4,9 +4,9 @@
 
 import os
 
-from bypy.constants import ismacos
+from bypy.constants import ismacos, PREFIX
 from bypy.utils import (cmake_build, install_binaries, iswindows,
-                        windows_cmake_build, build_dir)
+                        windows_cmake_build, build_dir, apply_patch)
 
 
 needs_lipo = True
@@ -14,7 +14,15 @@ needs_lipo = True
 
 def main(args):
     if iswindows:
-        windows_cmake_build(ENABLE_SHARED='FALSE')
+        # https://github.com/mozilla/mozjpeg/pull/440
+        apply_patch('mozjpeg-gh-440.patch', level=1)
+        windows_cmake_build(
+            BUILD_SHARED_LIBS='FALSE',
+            PNG_LIBRARY=os.path.join(PREFIX, 'lib', 'libpng16.lib'),
+            PNG_PNG_INCLUDE_DIR=os.path.join(PREFIX, 'include'),
+            ZLIB_INCLUDE_DIR=os.path.join(PREFIX, 'include'),
+            ZLIB_LIBRARY=os.path.join(PREFIX, 'lib', 'zdll.lib'),
+        )
         install_binaries('build/jpegtran-static.exe',
                          'bin',
                          fname_map=lambda x: 'jpegtran-calibre.exe')
