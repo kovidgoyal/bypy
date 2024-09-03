b063f1190dcb4f44e512eeeac4e3ee3c1573f0bb
Kovid Goyal <kovid@kovidgoyal.net>
2024-09-03 21:07:14 +0530

    Build script for piper

---
+++ bypy/pkgs/piper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2024, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+import os
+from bypy.constants import build_dir
+from bypy.utils import cmake_build
+
+
+def main(args):
+    cmake_build(override_prefix=os.path.join(build_dir(), 'piper'), relocate_pkgconfig=False)
