e9d652c7701a39076a0827a338615f667fdc1e0b
Kovid Goyal <kovid@kovidgoyal.net>
2024-09-04 17:24:39 +0530

    Fix perms on generated .so files

--- bypy/pkgs/piper.py
+++ bypy/pkgs/piper.py
@@ -7,8 +7,12 @@ from bypy.constants import build_dir, PREFIX
 from bypy.utils import cmake_build
 
 def main(args):
+    dest = os.path.join(build_dir(), 'piper')
     cmake_build(
-        override_prefix=os.path.join(build_dir(), 'piper'), relocate_pkgconfig=False,
+        override_prefix=dest, relocate_pkgconfig=False,
         CMAKE_VERBOSE_MAKEFILE='ON',
         PIPER_PHONEMIZE_DIR=os.path.join(PREFIX, 'piper-phonemize').replace(os.sep, '/'),
     )
+    for x in os.scandir(dest):
+        if '.so.' in x.name and not x.is_symlink():
+            os.chmod(x.path, 0o755)
