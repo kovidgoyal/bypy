ccd3dd23320b38d7743e5f556981ba7624b1e216
Kovid Goyal <kovid@kovidgoyal.net>
2024-09-04 12:26:26 +0530

    Build scripts for piper header only deps

---
+++ bypy/pkgs/piper_spdlog.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2024, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+import os
+from bypy.constants import build_dir
+from bypy.utils import cmake_build
+
+def main(args):
+    cmake_build(
+        override_prefix=os.path.join(build_dir(), 'piper-spdlog'),
+        # See CMakeLists.txt in piper for these settings
+    )
