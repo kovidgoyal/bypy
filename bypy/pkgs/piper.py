ca37507f18ad5bfcb1860f0cd349ba392416943b
Kovid Goyal <kovid@kovidgoyal.net>
2024-09-04 12:29:44 +0530

    Use prebuilt deps for piper

--- bypy/pkgs/piper.py
+++ bypy/pkgs/piper.py
@@ -3,9 +3,14 @@
 
 
 import os
-from bypy.constants import build_dir
+from bypy.constants import build_dir, PREFIX
 from bypy.utils import cmake_build
 
-
 def main(args):
-    cmake_build(override_prefix=os.path.join(build_dir(), 'piper'), relocate_pkgconfig=False)
+    cmake_build(
+        override_prefix=os.path.join(build_dir(), 'piper'), relocate_pkgconfig=False,
+        CMAKE_VERBOSE_MAKEFILE='ON',
+        FMT_DIR=os.path.join(PREFIX, 'piper-fmt').replace(os.sep, '/'),
+        SPDLOG_DIR=os.path.join(PREFIX, 'piper-spdlog').replace(os.sep, '/'),
+        PIPER_PHONEMIZE_DIR=os.path.join(PREFIX, 'piper-phonemize').replace(os.sep, '/'),
+    )
