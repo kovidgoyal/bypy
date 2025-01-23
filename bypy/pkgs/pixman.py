4971a33c6a2ad71228e62699b15bd550bdab9b9a
Kovid Goyal <kovid@kovidgoyal.net>
2025-01-23 08:33:47 +0530

    Code to build pixman and cairo

---
+++ bypy/pkgs/pixman.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+
+from bypy.utils import meson_build
+
+
+def main(args):
+    meson_build()
