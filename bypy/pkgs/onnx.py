ba4215d54db40de6d16233f8e789d225b523655f
Kovid Goyal <kovid@kovidgoyal.net>
2025-07-29 17:32:51 +0530

    Fix onnx build software requirements on mac/win

--- bypy/pkgs/onnx.py
+++ bypy/pkgs/onnx.py
@@ -2,6 +2,7 @@
 # License: GPLv3 Copyright: 2024, Kovid Goyal <kovid at kovidgoyal.net>
 
 import os
+import re
 
 from bypy.constants import CMAKE, PREFIX, PYTHON, UNIVERSAL_ARCHES, build_dir, ismacos, iswindows
 from bypy.utils import copy_headers, install_binaries, replace_in_file, run, run_shell
@@ -17,9 +18,14 @@ def main(args):
         arches = ';'.join(UNIVERSAL_ARCHES)
         cmdline += f' --cmake_extra_defines CMAKE_OSX_ARCHITECTURES={arches} CMAKE_VERBOSE_MAKEFILE=ON'
         kw['append_to_path'] = os.path.dirname(CMAKE)
+        kw['prepend_to_path'] = os.path.dirname(PYTHON)  # onnx build script requires python >= 3.10
     elif iswindows:
         replace_in_file('tools/ci_build/build.py', 'target_arch = platform.machine()', 'target_arch = platform.machine().upper()')
         kw['append_to_path'] = os.path.dirname(PYTHON)
+        # Visual Studio 2022 ships with ancient CMake, but we dont actually
+        # need updated cmake with one C++ 23 patch
+        replace_in_file('cmake/CMakeLists.txt', re.compile(r'cmake_minimum_required.+'), '')
+        replace_in_file('cmake/onnxruntime_common.cmake', re.compile(r'if.+cxx_std_23.+'), 'if(FALSE)')
     else:
         cmdline += f' --allow_running_as_root --cmake_extra_defines CMAKE_SYSTEM_PREFIX_PATH={PREFIX}'
     run(cmdline, **kw)
