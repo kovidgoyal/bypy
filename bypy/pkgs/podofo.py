36d99e794c2cb1621930752546876a7793028b6a
Kovid Goyal <kovid@kovidgoyal.net>
2025-09-21 14:43:01 +0530

    Fixing building of newer podofo

--- bypy/pkgs/podofo.py
+++ bypy/pkgs/podofo.py
@@ -10,12 +10,6 @@ def main(args):
     # want libpodofo to link against the system libtiff
     replace_in_file('CMakeLists.txt', 'if(TIFF_FOUND)', 'if(TIFF_FOUND_DISABLED)')
 
-    # Patches to build with newer versions of libxml2
-    # https://github.com/podofo/podofo/commit/d470a0a2ad423042c29db08ba13170f42d8806cf
-    replace_in_file('src/podofo/private/XmlUtils.h', '#include <libxml/tree.h>', '#include <libxml/tree.h>\n#include <libxml/xmlerror.h>')
-    replace_in_file('src/podofo/private/XmlUtils.h', 'xmlErrorPtr error_', 'const xmlError *error_')
-    replace_in_file('src/podofo/main/PdfXMPPacket.cpp', '#include <libxml/xmlsave.h>', '#include <libxml/xmlsave.h>\n#include <libxml/parser.h>')
-
     cmake_build(
         make_args='podofo_shared',
         PODOFO_BUILD_LIB_ONLY='TRUE',
