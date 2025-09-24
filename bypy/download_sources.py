fbc8521780919f146def52b323c36b2cc9b41da7
Kovid Goyal <kovid@kovidgoyal.net>
2025-09-24 13:52:44 +0530

    Fail if toolchain not found

--- bypy/download_sources.py
+++ bypy/download_sources.py
@@ -453,6 +453,8 @@ def read_go_deps() -> list[Dependency]:
                     name='go', ecosystem='go', version=v, cpe=cpe, purl=f'pkg:golang/std@{v}',
                     _spdx_license_id='BSD-3-Clause', urls=('https://go.dev/doc/install',)))
                 break
+        else:
+            raise ValueError('Failed to find toolchain in go.mod')
     with suppress(FileNotFoundError), open('go.sum') as f:
         for line in f:
             name, version, alg = line.split()
