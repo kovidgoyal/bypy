6bcaadaa4b1533c645d34cf54a1d7240fd99b182
Kovid Goyal <kovid@kovidgoyal.net>
2025-12-09 06:33:14 +0530

    Fix build of latest gnuwin32 on windows

--- bypy/pkgs/gnuwin32.py
+++ bypy/pkgs/gnuwin32.py
@@ -2,16 +2,15 @@
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-import glob
 import os
 
 from bypy.constants import build_dir
 
 
 def main(args):
+    srcdir = os.getcwd()
     os.chdir('..')
     dest = os.path.join(build_dir(), 'private')
     os.makedirs(dest)
-    srcdir = glob.glob('gnuwin32.zip-*')[0]
     os.rename(srcdir, os.path.join(dest, 'gnuwin32'))
     os.mkdir(srcdir)
